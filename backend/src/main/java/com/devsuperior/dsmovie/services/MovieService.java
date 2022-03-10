d8677a9dbf570f101fa6a17fb101bb1b5ba89439
danieldl0 <daniel.d.oliveira20@gmail.com>
2022-03-10 11:12:12 -0300

    find movies

---
+++ backend/src/main/java/com/devsuperior/dsmovie/services/MovieService.java
@@ -0,0 +1,34 @@
+package com.devsuperior.dsmovie.services;
+
+import java.util.List;
+
+import com.devsuperior.dsmovie.dto.MovieDTO;
+import com.devsuperior.dsmovie.entities.Movie;
+import com.devsuperior.dsmovie.repositories.MovieRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class MovieService {
+    
+    @Autowired
+    private MovieRepository repository;
+
+    @Transactional(readOnly = true)
+    public Page<MovieDTO> findAll(Pageable pageable){
+    	Page <Movie> result = repository.findAll(pageable);
+        Page <MovieDTO> allMovies = result.map(movie -> new MovieDTO(movie));
+    	return allMovies;
+    }
+
+    public MovieDTO findById(Long id){
+        Movie res = repository.findById(id).get();
+        MovieDTO movie = new MovieDTO(res);
+        return movie;
+    }
+
+}
