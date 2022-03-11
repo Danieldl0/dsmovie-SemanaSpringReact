e60b71517b92ee58a6f071889929610cd83c19c7
danieldl0 <daniel.d.oliveira20@gmail.com>
2022-03-11 12:16:46 -0300

    Props

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -1,25 +1,22 @@
 import { MovieScore } from "components/MovieScore";
 import "./style.css";
 import { Link } from "react-router-dom";
+import { Movie } from "types/typesmovies";
 
-function MovieCard(){
-    
-    const movie1 = {
-        id:7,
-        imagem: "https://image.tmdb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
-        title: "The witcher",
-        count: 2,
-        score: 4.5
-    };
-    
-    return(
+type Props ={
+    movie: Movie
+}
+
+function MovieCard({ movie }: Props) {
+
+    return (
 
         <div className="dsmovie-card-container">
-            <img className="dsmovie-card-img" src={movie1.imagem} alt="" />
-            <h3>{movie1.title}</h3>
-            <MovieScore/>
-            <Link to={`/form/${movie1.id}`}><button className="btn btn-primary dsmovie-btn">Avaliar</button></Link>
-            
+            <img className="dsmovie-card-img" src={movie.image} alt="" />
+            <h3>{movie.title}</h3>
+            <MovieScore />
+            <Link to={`/form/${movie.id}`}><button className="btn btn-primary dsmovie-btn">Avaliar</button></Link>
+
         </div>
     );
 
@@ -27,4 +24,4 @@ function MovieCard(){
 }
 
 
-export {MovieCard};
+export { MovieCard };
--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,3 +1,4 @@
+import { isContentEditable } from "@testing-library/user-event/dist/utils";
 import axios from "axios";
 import { MovieCard } from "components/MovieCard";
 import { Pagination } from "components/Pagination"
@@ -22,34 +23,33 @@ export function Listing() {
 
     const [numberPage, setNumberPage] = useState(0);
 
-
     useEffect(() => {
-        axios.get(`${BASE_URL}/movies?size=12&page=${numberPage}`)
+        axios.get(`${BASE_URL}/movies?size=12&page=${numberPage}&sort=title`)
             .then(response => {
                 const data = response.data as MoviePage
                 console.log(response.data);
                 setPage(data);
-                })
+            })
             .catch(erro => console.log("erro" + erro));
-    }, []);
+    }, [numberPage]);
+
 
     return (
         <>
             <Pagination />
             <div className="container">
                 <div className="row">
-                    <div className="col-sm-6 col-md-4 col-xl-3 mb-3" >
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-md-4 col-xl-3 mb-3" >
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-md-4 col-xl-3 mb-3" >
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-md-4 col-xl-3 mb-3" >
-                        <MovieCard />
-                    </div>
+
+                    {page.content.map(movie => ( 
+                        <div key={movie.id} className="col-sm-6 col-md-4 col-xl-3 mb-3" >
+                            <MovieCard
+                                movie = {movie}
+                            />
+                        </div>
+                    ))}
+
+
+
                 </div>
 
             </div>
