b21dddf770577fd25c7cffe116c984db9e20cfd4
danieldl0 <daniel.d.oliveira20@gmail.com>
2022-03-11 18:08:52 -0300

    Save, useNavigate

--- frontend/src/components/FormCard/FormCard.tsx
+++ frontend/src/components/FormCard/FormCard.tsx
@@ -1,48 +1,26 @@
-import { Link } from "react-router-dom";
-import { Movie } from "types/typesmovies";
 import "./style.css";
 
-type Props = {
-    movie: Movie;
-}
 
-function FormCard({movie}: Props) {
+
+function FormCard() {
 
     return (
-        <div className="dsmovie-form-container">
-            <img className="dsmovie-card-img" src={movie?.image} alt={movie.title} />
-            <div className="dsmovie-form-container-bottom">
-                <h3>{movie.title}</h3>
-                <form className="dsmovie-form-inputs">
-                    <div className="form-group dsmovie-div-input">
-                        <label>Informe seu Email</label>
-                        <input type="email" className="form-control" id="email" />
-                    </div>
-                    <div className="form-group dsmovie-div-input">
-                        <label> Informe sua pontuação</label>
-                        <select className="form-control">
-                            <option>1</option>
-                            <option>2</option>
-                            <option>3</option>
-                            <option>4</option>
-                            <option>5</option>
-                        </select>
-                    </div>
-                    <div className="dsmovie-btn-container">
-                        <Link to="/">
-                            <button className="btn btn-primary dsmovie-btn">Salvar</button>
-                        </Link>
-                    </div>
-                </form>
-
-                <div className="dsmovie-btn-container">
-                    <Link to="/">
-                        <button className="btn btn-primary dsmovie-btn"> Cancelar</button>
-                    </Link>
-                </div>
+        <>
+            <div className="form-group dsmovie-form-div-input">
+                <label>Informe seu Email</label>
+                <input type="email" className="form-control" id="email" name="email" />
             </div>
-        </div>
+            <div className="form-group dsmovie-form-div-input">
+                <label> Informe sua pontuação</label>
+                <select className="form-control" id="score">
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                </select>
+            </div>
+        </>
     )
 }
-
-export {FormCard};
+export { FormCard };
--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,15 +1,19 @@
-import { useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import { FormCard } from "components/FormCard/FormCard";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { BASE_URL } from "utils/requests";
 import { Movie } from "types/typesmovies";
+import { validateEmail } from "utils/validateEmail";
 
 
 
 export function Form() {
 
     const params = useParams();
+    const navigate = useNavigate();
+
+    
 
     const [movie, setMovie] = useState<Movie>({
         id: 0,
@@ -19,16 +23,57 @@ export function Form() {
         image: "",
     });
 
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+
+        event.preventDefault();
+
+        const email = (event.target as any).email.value;
+        const score = (event.target as any).score.value;
+
+        if(!validateEmail(email)){
+            return;
+        }
+
+        axios.put(`${BASE_URL}/scores/`, {
+            email: email,
+            score: score,
+            movieId: movie.id
+        }).then(resp => {
+            navigate("/");
+        }).catch(err => {
+            console.log("Erro ao cadastrar nota");
+        })
+    }
+
 
     useEffect(() => {
         axios.get(`${BASE_URL}/movies/${params.idmovie}`)
             .then(resp => {
                 setMovie(resp.data)
+            }).catch(() => {
+                alert("Filme não cadastrado")
+                navigate("/");
             });
-    }, [params.idmovie]);
+    }, [params.idmovie, navigate]);
 
     return (
-        <FormCard movie={movie} />
+        <div className="dsmovie-form-container">
+            <img className="dsmovie-card-img" src={movie.image} alt={movie.title} />
+            <div className="dsmovie-form-container-bottom">
+                <h3>{movie.title}</h3>
+                <form onSubmit={handleSubmit} className="dsmovie-form-inputs">
+                    <FormCard />
+                    <div className="dsmovie-form-btn-container">
+                        <button className="btn btn-primary dsmovie-btn">Salvar</button>
+                    </div>
+                </form>
+                <div className="dsmovie-form-btn-container">
+                    <Link to="/">
+                        <button className="btn btn-primary dsmovie-btn"> Cancelar</button>
+                    </Link>
+                </div>
+            </div>
+        </div>
 
     );
 }
